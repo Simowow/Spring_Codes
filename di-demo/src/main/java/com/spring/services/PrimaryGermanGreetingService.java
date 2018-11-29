0252cd60f0ea7a587c040dd889fe970933269912
Simoneven120 <simon.kiflerman@appian.pro>
2018-11-28 23:13:59 -0500

    Commit

---
+++ di-demo/src/main/java/com/spring/services/PrimaryGermanGreetingService.java
@@ -0,0 +1,18 @@
+package com.spring.services;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Service
+@Primary
+@Profile("de")
+public class PrimaryGermanGreetingService implements GreetingServices {
+
+	@Override
+	public String sayGreeting() {
+		
+		return "Primarer GruBdienst!";
+	}
+
+}
